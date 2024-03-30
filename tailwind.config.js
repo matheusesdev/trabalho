5e4c72df2bfac4e669b697265d79ddf74a7a2d69
Matheus <matheuses18@icloud.com>
2024-03-30 11:32:56 -0300

    adicionei video

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
 
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+      extend: {
+        aspectRatio: {
+          "4/3": "4 / 3",
+        },
+      },
+      plugins: [],
+    },
+  },
+};
